5dac3c7cc17afc9aa273d164976f86fa48b692e7
Oron <136211946+oron01@users.noreply.github.com>
2024-03-05 03:04:58 +0200

    complete fully functioning project

--- code.js
+++ code.js
@@ -94,6 +94,15 @@ function buildTree(array) {
         }
     }
 
+let findNode = (value,currentNode) => {
+    if (value == currentNode.data) {
+        alert(currentNode)
+        return currentNode}
+    else if (value > currentNode.data && currentNode.right !== null) {return findNode(value,currentNode.right)}
+    else if (value < currentNode.data && currentNode.left !== null) {return findNode(value,currentNode.left)}
+    else {return null}
+}
+
     let removeValue = (value,node) => {
         let findNode = (value,node) => {
             if (value == node.data) return node
@@ -134,6 +143,180 @@ function buildTree(array) {
 
     }
 
+    const levelOrder = (callback=null,node) => {
+        let nodeList = []
+        let visitList = [node]
+        let getNodeList = (node,visitList) => {
+        nodeList.push(node)
+        let leftChild
+        let rightChild
+        console.log(node)
+        if (node.right && node.left) {
+            leftChild = node.left
+            rightChild = node.right
+            visitList.push(leftChild)
+            visitList.push(rightChild)
+        }
+        else if (node.right !== null) {
+            rightChild = node.right
+            visitList.push(rightChild)
+        }
+        else if (node.left !== null) {
+            leftChild = node.left
+            visitList.push(leftChild)
+        }
+        visitList.shift()
+        if (visitList[0]) {
+        getNodeList(visitList[0],visitList)}
+    }
+    getNodeList(node,visitList)
+    if (callback !== null) {
+    callback(nodeList)}
+    else {return nodeList}
+
+    }
+
+let preOrderTransversal = (node) => {
+    let nodeList = []
+    let getNodeList = (node) => {
+        nodeList.push(node)
+        if (node.left) {
+            getNodeList(node.left)
+        }
+        if (node.right) {
+            getNodeList(node.right)
+    }
+}
+    getNodeList(node)
+    console.log(nodeList)
+    return nodeList
+}
+
+let postOrderTransversal = (node) => {
+    let nodeList = []
+    let getNodeList = (node) => {
+        if (node.left) {
+            getNodeList(node.left)
+        }
+        if (node.right) {
+            getNodeList(node.right)}
+        nodeList.push(node)
+    }
+    getNodeList(node)
+    console.log(nodeList)
+    return nodeList
+}
+
+let inOrderTransversal = (node) => {
+    let nodeList = []
+    let getNodeList = (node) => {
+        if (node.left) {
+            getNodeList(node.left)
+        }
+        nodeList.push(node)
+        if (node.right) {
+            getNodeList(node.right)
+        }
+    }
+    getNodeList(node)
+    console.log(nodeList)
+    return nodeList
+}
+
+const depth = (node) => {
+    let depth = 0
+    let transverseTree = (currentNode) => {
+        if (currentNode.data > node.data && currentNode.left) {
+            depth += 1
+            transverseTree(currentNode.left)
+        }
+        else if (currentNode.data < node.data && currentNode.right) {
+            depth += 1
+            transverseTree(currentNode.right)
+        }
+        else if (currentNode.data == node.data) {
+            alert("babonag")
+            return depth
+        }
+        else {
+            alert(`${currentNode.data} ${node.data}`)
+            throw console.error("node doesn't exist")};
+    }
+    transverseTree(first)
+    return depth
+
+}
+
+const height = (currentNode,targetNode) => {
+    let totalHeight = 0
+    let leafNode
+    let getHeightValue = (currentNode) => {
+        if (!currentNode) {console.error("Doesn't exist")}
+        else if (currentNode.data > targetNode.data) {
+            leftVal = getHeightValue(currentNode.left)
+        }
+        else if (currentNode.data < targetNode.data) {
+            rightVal = getHeightValue(currentNode.right)
+        }
+        else if (currentNode.data == targetNode.data) {
+            let transverse = (node,height=0) => {
+                if (node.right) {
+                    transverse(node.right,height+1)
+                }
+                if (node.left) {
+                    transverse(node.left,height+1)
+                }
+                else if (!node.left && !node.right) {
+                    if (height > totalHeight) {
+                        totalHeight = height
+                        leafNode = node
+                    }
+                }
+            }
+            transverse(currentNode)
+        }
+    }
+    getHeightValue(currentNode)
+    return totalHeight
+
+}
+
+const isBalanced = (node) => {
+    let balance = true
+    let getBalanceValue = (currentNode,balanced=true) => {
+        let rightVal = 0
+        let leftVal = 0
+
+        if (balanced == false) {return balance = false}
+        if (currentNode.left) {
+            leftVal = getBalanceValue(currentNode.left)
+            leftVal += 1
+        }
+        if (currentNode.right) {
+            rightVal = getBalanceValue(currentNode.right)
+            rightVal += 1
+        }
+        console.log(rightVal,leftVal)
+        if (rightVal > leftVal + 1 || leftVal > rightVal + 1) {
+            balance = false
+        }
+        return rightVal + leftVal
+        }
+        getBalanceValue(node)
+        return balance
+    }
+
+const rebalance = (node) => {
+    let array = inOrderTransversal(node)
+    let newArr = []
+    for (let i = 0; i < array.length ; i++) {
+        newArr[i] = array[i].data 
+    }
+    console.log(newArr)
+    let balancedTree = buildTree(newArr)
+    return balancedTree
+}
+
     const prettyPrint = (node, prefix = "", isLeft = true) => {
         if (node === null) {
           return;
@@ -149,8 +332,17 @@ function buildTree(array) {
       
       let first = createBST(sortedList)
       prettyPrint(first)
-      removeValue(5,first)
+    //   removeValue(5,first)
+      insertValue(3,first)
       prettyPrint(first)
+      let obama = levelOrder(null,first)
+      let borrack = preOrderTransversal(first)
+      let dat = postOrderTransversal(first)
+      let president = inOrderTransversal(first)
+      let lol = depth(findNode(3,first),first)
+      let balamciaga = isBalanced(first)
+      prettyPrint(first)
+      return {prettyPrint,findNode,insertValue,first,obama,borrack,dat,president,lol,balamciaga,rebalance,isBalanced,height}
 }
 
 /*
